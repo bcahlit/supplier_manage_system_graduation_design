a5d65ffa7dd4e3acc2fde4602124c6746d93945d
bcahlit <bcahlit@gmail.com>
2019-05-12 18:08:44 +0800

    basic chart demo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,12 +16,14 @@ import { frameInRoutes } from '@/router/routes'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import D2Crud from '@d2-projects/d2-crud'
-
+// [ 可选组件 ] 图表
+import VCharts from 'v-charts'
 
 // 核心插件
 Vue.use(d2Admin)
 Vue.use(ElementUI)
 Vue.use(D2Crud)
+Vue.use(VCharts)
 
 new Vue({
   router,
--- frontend/src/pages/data_analysis/supplier/index.vue
+++ frontend/src/pages/data_analysis/supplier/index.vue
@@ -1,7 +1,67 @@
 <template>
   <d2-container>
-    <template slot="header">header</template>
-    供应商分析
-    <template slot="footer">footer</template>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <ve-pie :data="chartData"></ve-pie>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light">
+          <ve-pie :data="chartData"></ve-pie>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <ve-pie :data="chartData"></ve-pie>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light">
+          <ve-pie :data="chartData"></ve-pie>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <ve-histogram :data="chartData"></ve-histogram>
+    </el-row>
   </d2-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        orderChartData: {
+          columns: ['公司', '订单数'],
+          rows: [
+            { '公司': '1/1', '订单数': 1393 },
+            { '公司': '1/2', '订单数': 3530 },
+            { '公司': '1/3', '订单数': 2923 },
+            { '公司': '1/4', '订单数': 1723 },
+            { '公司': '1/5', '订单数': 3792 },
+            { '公司': '1/4', '订单数': 1723 },
+            { '公司': '1/5', '订单数': 3792 },
+            { '公司': '1/6', '订单数': 4593 }
+          ]
+        },
+        chartData: {
+          columns: ['公司', '订单数'],
+          rows: [
+            { '公司': '1/1', '订单数': 1393 },
+            { '公司': '1/2', '订单数': 3530 },
+            { '公司': '1/3', '订单数': 2923 },
+            { '公司': '1/7', '订单数': 1393 },
+            { '公司': '1/8', '订单数': 3530 },
+            { '公司': '1/9', '订单数': 2923 },
+            { '公司': '1/4', '订单数': 1723 },
+            { '公司': '1/5', '订单数': 3792 },
+            { '公司': '1/6', '订单数': 4593 }
+          ]
+        }
+      }
+    }
+  }
+</script>
