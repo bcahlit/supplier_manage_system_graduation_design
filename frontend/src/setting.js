3c5064bb5022ee72524a0b246bb8ff9642b7180e
bcahlit <bcahlit@gmail.com>
2019-05-20 19:46:55 +0800

    add inner plan picture

--- frontend/src/setting.js
+++ frontend/src/setting.js
@@ -34,28 +34,25 @@ export default {
       {
         title: 'd2admin 经典',
         name: 'd2',
+        backgroundImage: 'image/bg.jpg',
         preview: 'image/theme/d2/preview@2x.png'
       },
       {
         title: '紫罗兰',
         name: 'violet',
+        backgroundImage: 'image/bg.jpg',
         preview: 'image/theme/violet/preview@2x.png'
-      },
-      {
-        title: '简约线条',
-        name: 'line',
-        backgroundImage: 'image/theme/line/bg.jpg',
-        preview: 'image/theme/line/preview@2x.png'
       },
       {
         title: '流星',
         name: 'star',
-        backgroundImage: 'image/theme/star/bg.jpg',
+        backgroundImage: 'image/bg.jpg',
         preview: 'image/theme/star/preview@2x.png'
       },
       {
         title: 'Tomorrow Night Blue (vsCode)',
         name: 'tomorrow-night-blue',
+        backgroundImage: 'image/bg.jpg',
         preview: 'image/theme/tomorrow-night-blue/preview@2x.png'
       }
     ]
